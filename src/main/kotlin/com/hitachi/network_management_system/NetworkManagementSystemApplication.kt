fbaa0f4db52e384ee8ca1f77fd316e79728b34f3
amkar11 <acmyr97@gmail.com>
2026-04-22 17:13:54 +0200

    initial

---
+++ src/main/kotlin/com/hitachi/network_management_system/NetworkManagementSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.hitachi.network_management_system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NetworkManagementSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<NetworkManagementSystemApplication>(*args)
+}
